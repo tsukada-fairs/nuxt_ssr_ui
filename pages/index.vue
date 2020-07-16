64697a95cd596d5523571bcf4e0652851c3d645a
naoyuki_tsukada <tsukada@fairs.jp>
2020-07-16 10:33:33 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        {{message}}
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      message: 'hello',
+      posts: {}
+    }
+  },
+  async asyncData({$axios}){
+    // 取得先のURL
+    // const url = "https://qiita.com/api/v2/items";
+    const url = "http://api:8000/api/interview";
+    // リクエスト（Get）
+    const response = await $axios.$get(url);
+    // 配列で返ってくるのでJSONにして返却
+    return {
+      posts: response
+    }
+  },
+  mounted() {
+    console.log(this.posts);
+  }
+})
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
